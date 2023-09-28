662fa02454bc04546674c0716bb0e516f7446bbf
Martin Eide <oenesmartin@msn.com>
2023-09-28 21:56:58 +0200

    Setup Svelte page structure
    Modify and change the default structure of a default Svelte project.
    Removed things that felt were not needed to start with. Very minimal.

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  import Test from './lib/Test.svelte';
+</script>
+
+<main>
+  <div class="card">
+    <Test/>
+    <Counter/>
+    <Test/>
+  </div>
+</main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Test from "./Test.svelte";
+  let count = -0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
