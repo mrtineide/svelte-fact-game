cfbd12d54d2f81dc856e6f206417b82a191fbc79
Martin Eide <oenesmartin@msn.com>
2023-10-01 01:01:55 +0200

    Add card selection logic & styling to fact cards
    Also changed the cardgame elements to be a part of a fieldset, with
    labels since it seem like it fit better than using "raw" buttons and
    checkboxes.
    (I always would like to use standard/semantic html elements when they
    seem to fit the webpage)

--- src/lib/CardGame.svelte
+++ src/lib/CardGame.svelte
@@ -4,26 +4,33 @@
     let round = 1;
     const topics = [
         {
-                facts: ["I am fun", "I am Super fun", "I am fun and cool"],
+            facts: ["I am fun", "I am Super fun", "I am fun and cool"],
         },
         {
-                facts: ["I am fun2", "I am Super fun2", "I am fun and cool2"],
+            facts: ["I am fun2", "I am Super fun2", "I am fun and cool2"],
         },
         {
-                facts: ["I am fun3", "I am Super fun3", "I am fun and coo3l"],
+            facts: ["I am fun3", "I am Super fun3", "I am fun and coo3l"],
         },
         {
-                facts: ["I am fun4", "I am Super 54fun", "I am fun and54 cool"],
-        }
+            facts: ["I am fun4", "I am Super 54fun", "I am fun and54 cool"],
+        },
     ];
 </script>
 
 <p>This is the fun fact game</p>
 
-<ul>
+<fieldset>
+    <legend>
+        <p>(put theme here?)</p>
+    </legend>
     {#each topics.at(round).facts as fact}
-        <li>
-            <FactCard {fact} />
-        </li>
+        <FactCard {fact} />
     {/each}
-</ul>
+</fieldset>
+
+<style>
+    fieldset{
+        display: grid;
+    }
+</style>
--- src/lib/FactCard.svelte
+++ src/lib/FactCard.svelte
@@ -1,8 +1,39 @@
 <script>
     export let fact;
+
+    let selected = false;
+
+    function clickedButton() {
+        selected = !selected;
+    }
 </script>
 
-<!---
-    <p>Fun Fact</p>
--->   
-<p>{fact}</p>
+<label>
+    <button
+        class={selected ? "selected-button" : "notSelected-button"}
+        on:click={clickedButton}
+    >
+        <input type="checkbox" bind:checked={selected} />
+        {fact}
+    </button>
+</label>
+
+<style>
+    button{
+        padding: auto;
+    }
+    button:hover {
+        box-shadow: 0 6px 10px var(--BoxShaddowColour);
+    }
+    .selected-button {
+        --NotSelectedColour: green;
+        --BoxShaddowColour: var(--NotSelectedColour);
+        background-color: var(--NotSelectedColour);
+    }
+
+    .notSelected-button {
+        --NotSelectedColour: red;
+        --BoxShaddowColour: var(--NotSelectedColour);
+        background-color: var(--NotSelectedColour);
+    }
+</style>
